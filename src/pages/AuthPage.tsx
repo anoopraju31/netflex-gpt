15c71bba517d61a11d0c203a81bf925c73a9b8ff
anoopraju31 <anoopraju31@gmail.com>
2023-11-18 10:45:19 +0530

    Auth Page - background image added

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
 const AuthPage = () => {
-	return <div>AuthPage</div>
+	return (
+		<main className='relative'>
+			<section className='w-full h-screen'>
+				<img
+					className='w-full h-full object-cover '
+					src='https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg'
+					alt='login background'
+				/>
+			</section>
+		</main>
+	)
 }
 
 export default AuthPage
