dcf63049be598b561cb9a72f4a479f16a64e4bb7
anoopraju31 <anoopraju31@gmail.com>
2023-11-18 23:14:41 +0530

    Movie API Slice - refactor

--- src/features/movieApi.ts
+++ src/features/movieApi.ts
@@ -1,5 +1,35 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+interface Dates {
+	maximum: string
+	minimum: string
+}
+
+interface Movie {
+	adult: boolean
+	backdrop_path: string
+	genre_ids: number[]
+	id: number
+	original_language: string
+	original_title: string
+	overview: string
+	popularity: number
+	poster_path: string
+	release_date: string
+	title: string
+	video: boolean
+	vote_average: number
+	vote_count: number
+}
+
+interface MovieResponse {
+	dates: Dates
+	page: number
+	results: Movie[]
+	total_pages: number
+	total_results: number
+}
+
 const movieApi = createApi({
 	reducerPath: 'movieApi',
 	baseQuery: fetchBaseQuery({
@@ -13,8 +43,16 @@ const movieApi = createApi({
 		},
 	}),
 	endpoints: (builder) => ({
-		getNowPlayingMovies: builder.query({
-			query: (page: number) => `/now_playing?page=${page}`,
+		getNowPlayingMovies: builder.query<Movie[], number | void>({
+			query: (page: number = 1) => `/now_playing?page=${page}`,
+			transformResponse: (res: MovieResponse) => res?.results,
+			serializeQueryArgs: ({ endpointName }) => endpointName,
+			merge: (currentCache, newItems: Movie[]) => {
+				currentCache.push(...newItems)
+			},
+			forceRefetch({ currentArg, previousArg }) {
+				return currentArg !== previousArg
+			},
 		}),
 	}),
 })
