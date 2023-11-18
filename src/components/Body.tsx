f352a3b8e4f1b862b68816f2d1d87fb2a4bb7117
anoopraju31 <anoopraju31@gmail.com>
2023-11-18 10:39:10 +0530

    Body Component - styled

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import { Outlet } from 'react-router-dom'
 import Header from './Header'
 
 const Body = () => {
 	return (
-		<>
+		<div className='bg-black min-h-screen'>
 			<Header />
-		</>
+			<Outlet />
+		</div>
 	)
 }
 
