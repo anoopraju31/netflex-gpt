eff865723ae15434aaa10a8d87820dd9971c0557
anoopraju31 <anoopraju31@gmail.com>
2023-11-18 14:23:05 +0530

    Hero Movie Container - initial

---
+++ src/components/HeroMovieContainer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const HeroMovieContainer = () => {
+	return <div>HeroMovieContainer</div>
+}
+
+export default HeroMovieContainer
--- src/pages/BrowsePage.tsx
+++ src/pages/BrowsePage.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import HeroMovieContainer from '../components/HeroMovieContainer'
 
 const BrowsePage = () => {
-	return <div>BrowsePage</div>
+	return (
+		<main>
+			<HeroMovieContainer />
+		</main>
+	)
 }
 
 export default BrowsePage
