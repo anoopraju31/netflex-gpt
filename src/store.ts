551a479221e37adcda6a5b849216938683714df8
anoopraju31 <anoopraju31@gmail.com>
2023-11-18 21:48:50 +0530

    Movie API Slice added to the store

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './features/userSlice'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import userReducer from './features/userSlice'
+import movieApi from './features/movieApi'
 
 const store = configureStore({
 	reducer: {
 		user: userReducer,
+		[movieApi.reducerPath]: movieApi.reducer,
 	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(movieApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
