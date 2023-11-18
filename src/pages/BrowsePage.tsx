16cda41438bcc6d6418443ff2e4298772fa4ea54
anoopraju31 <anoopraju31@gmail.com>
2023-11-18 14:24:21 +0530

    Secondary Movie Container - initial

--- src/pages/BrowsePage.tsx
+++ src/pages/BrowsePage.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import HeroMovieContainer from '../components/HeroMovieContainer'
+import SecondaryMovieContainer from '../components/SecondaryMovieContainer'
 
 const BrowsePage = () => {
 	return (
 		<main>
 			<HeroMovieContainer />
+			<SecondaryMovieContainer />
 		</main>
 	)
 }
