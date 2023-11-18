22ef043305b291742038f616e78c542514c60ed4
anoopraju31 <anoopraju31@gmail.com>
2023-11-18 10:46:37 +0530

    Auth Page - background image added to constant

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { BG_IMG } from '../utills/constants'
 
 const AuthPage = () => {
 	return (
@@ -6,7 +6,7 @@ const AuthPage = () => {
 			<section className='w-full h-screen'>
 				<img
 					className='w-full h-full object-cover '
-					src='https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg'
+					src={BG_IMG}
 					alt='login background'
 				/>
 			</section>
