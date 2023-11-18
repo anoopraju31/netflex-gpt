e26a3cbdbcca0dd1ce81a5ca0eaeaa9d0dbded7a
anoopraju31 <anoopraju31@gmail.com>
2023-11-19 00:33:46 +0530

    Hero Movie Container - Video Title and Background Video added

--- src/components/HeroMovieContainer.tsx
+++ src/components/HeroMovieContainer.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import VideoTitle from './VideoTitle'
+import BackgroundVideo from './BackgroundVideo'
 
 const HeroMovieContainer = () => {
-	return <div>HeroMovieContainer</div>
+	return (
+		<section className=''>
+			<VideoTitle />
+			<BackgroundVideo />
+		</section>
+	)
 }
 
 export default HeroMovieContainer
