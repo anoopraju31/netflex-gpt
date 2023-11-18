f5cd4d1b63054ff8e66c140f98ee09513a154701
anoopraju31 <anoopraju31@gmail.com>
2023-11-18 13:18:44 +0530

    Header Component - logout functionality added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,15 @@
+import { useNavigate } from 'react-router-dom'
 import { LOGO_IMG } from '../utills/constants'
+import { signOut } from 'firebase/auth'
+import { auth } from '../utills/firebase'
 
 const Header = () => {
+	const navigate = useNavigate()
+	const handleSignOut = () => {
+		signOut(auth)
+			.then(() => navigate('/'))
+			.catch((error) => navigate('/error'))
+	}
 	return (
 		<header className='absolute w-full px-0 sm:px-8 py-2 bg-gradient-to-b from-black z-50'>
 			<div className=''>
@@ -9,6 +18,14 @@ const Header = () => {
 					src={LOGO_IMG}
 					alt='logo'
 				/>
+			</div>
+
+			<div className=''>
+				<button
+					onClick={handleSignOut}
+					className='py-1 px-4 text-white bg-red-600 rounded-lg font-medium'>
+					Sign Out
+				</button>
 			</div>
 		</header>
 	)
