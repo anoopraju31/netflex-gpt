fdb6ec6b7bfc065b38dac083aee8d93e19a54a20
anoopraju31 <anoopraju31@gmail.com>
2023-11-19 11:29:00 +0530

    Hero Movie Container - fetched movie data

--- src/components/HeroMovieContainer.tsx
+++ src/components/HeroMovieContainer.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import VideoTitle from './VideoTitle'
 import BackgroundVideo from './BackgroundVideo'
+import { useGetNowPlayingMoviesQuery } from '../features/movieApi'
 
 const HeroMovieContainer = () => {
+	const { data: movies } = useGetNowPlayingMoviesQuery(1)
+
+	if (!movies) return <div></div>
+
+	const movie = movies[0]
+	const { id, backdrop_path } = movie
 	return (
 		<section className=''>
 			<VideoTitle />
-			<BackgroundVideo />
+			<BackgroundVideo movieId={id} posterId={backdrop_path} />
 		</section>
 	)
 }
