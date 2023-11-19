62287168c51b25550da755a8784c927c0abb745f
anoopraju31 <anoopraju31@gmail.com>
2023-11-19 18:37:52 +0530

    Search Page - Background added

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { BG_IMG } from '../utills/constants'
 
 const SearchPage = () => {
-	return <div>SearchPage</div>
+	return (
+		<div className='relative'>
+			<div className='fixed top-0 left-0 right-0 w-full h-screen'>
+				<img
+					className='w-full h-full object-cover'
+					src={BG_IMG}
+					alt='login background'
+				/>
+			</div>
+		</div>
+	)
 }
 
 export default SearchPage
