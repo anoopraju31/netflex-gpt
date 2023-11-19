8265c7988cf8b5edf48e9a502139e7e7c68bac5e
anoopraju31 <anoopraju31@gmail.com>
2023-11-19 14:51:15 +0530

    Move Card Component - placeholder added

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,3 +1,4 @@
+import { FaImage } from 'react-icons/fa6'
 import { IMG_CDN_URL } from '../utills/constants'
 
 interface MovieCardProps {
@@ -9,11 +10,17 @@ const MovieCard = (props: MovieCardProps) => {
 	const { posterId, title } = props
 	return (
 		<div className='w-36 md:w-48 relative group flex-shrink-0 cursor-pointer flex'>
-			<img
-				className='rounded-xl w-full'
-				src={IMG_CDN_URL + posterId}
-				alt={title}
-			/>
+			{posterId ? (
+				<img
+					className='rounded-xl w-full'
+					src={IMG_CDN_URL + posterId}
+					alt={title}
+				/>
+			) : (
+				<div className='w-full h-full flex justify-center bg-black/70 border rounded-xl items-center text-gray-300'>
+					<FaImage size={64} />
+				</div>
+			)}
 
 			<div className='absolute w-full h-full group-hover:bg-black/40'></div>
 		</div>
