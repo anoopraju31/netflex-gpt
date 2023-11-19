7cbc5cbf385256c149e8157a0900dec6a9a982db
anoopraju31 <anoopraju31@gmail.com>
2023-11-19 14:27:47 +0530

    Secondary Movie Container Compoenent - Movie Lists added

--- src/components/SecondaryMovieContainer.tsx
+++ src/components/SecondaryMovieContainer.tsx
@@ -1,12 +1,24 @@
-import { useGetNowPlayingMoviesQuery } from '../features/movieApi'
+import {
+	useGetNowPlayingMoviesQuery,
+	useGetPopularMoviesQuery,
+	useGetTopRatedMoviesQuery,
+	useGetTrendingMoviesQuery,
+} from '../features/movieApi'
 import MovieList from './MovieList'
 
 const SecondaryMovieContainer = () => {
-	const { data: nowPlaying } = useGetNowPlayingMoviesQuery(1)
+	const { data: nowPlayingMovies } = useGetNowPlayingMoviesQuery(1)
+	const { data: trendingMovies } = useGetTrendingMoviesQuery(1)
+	const { data: popularMovies } = useGetPopularMoviesQuery(1)
+	const { data: topRatedMovies } = useGetTopRatedMoviesQuery(1)
 
 	return (
-		<div>
-			<MovieList title='Now Playing' movies={nowPlaying} />
+		<div className='relative w-full md:-top-10 lg:-top-28 xl:-top-36 z-50'>
+			<MovieList title='Now Playing' movies={nowPlayingMovies} />
+			<MovieList title='Trending' movies={trendingMovies} />
+			<MovieList title='Popular' movies={popularMovies} />
+			<MovieList title='Top Rated' movies={topRatedMovies} />
+			<MovieList title='Now Playing' movies={nowPlayingMovies} />
 		</div>
 	)
 }
