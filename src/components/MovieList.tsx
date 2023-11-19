b20703a1732d634ffae6a2ad6f5a11c5a00f7bed
anoopraju31 <anoopraju31@gmail.com>
2023-11-19 14:04:15 +0530

    MoveList Compoenent - heading added

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -7,7 +7,16 @@ interface MovieListProps {
 
 const MovieList = (props: MovieListProps) => {
 	const { title, movies } = props
-	return <div>MovieList</div>
+	return (
+		<section className='text-white'>
+			<div className='p-4'>
+				<h2 className='text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold'>
+					{title}
+				</h2>
+			</div>
+			<div className=''></div>
+		</section>
+	)
 }
 
 export default MovieList
