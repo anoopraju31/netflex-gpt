e55ef76de88058bc3c855d987be78d3eb65cbab9
anoopraju31 <anoopraju31@gmail.com>
2023-11-19 17:14:06 +0530

    Header component - refactor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,23 +18,25 @@ const Header = () => {
 	}
 	return (
 		<header className='absolute w-full px-0 sm:px-8 py-2 bg-gradient-to-b from-black z-50'>
-			<div className=''>
-				<img
-					className='w-28 sm:w-32 md:w-36 lg:w-44'
-					src={LOGO_IMG}
-					alt='logo'
-				/>
-			</div>
-
-			{user && (
+			<div className='max-w-[1536px] mx-auto flex flex-col md:flex-row justify-between'>
 				<div className=''>
-					<button
-						onClick={handleSignOut}
-						className='py-1 px-4 text-white bg-red-600 rounded-lg font-medium'>
-						Sign Out
-					</button>
+					<img
+						className='w-28 sm:w-32 md:w-36 lg:w-44'
+						src={LOGO_IMG}
+						alt='logo'
+					/>
 				</div>
-			)}
+
+				{user && (
+					<div className='hidden md:flex items-center gap-2'>
+						<button
+							onClick={handleSignOut}
+							className='py-1 px-4 text-white bg-red-600 rounded-lg font-medium'>
+							Sign Out
+						</button>
+					</div>
+				)}
+			</div>
 		</header>
 	)
 }
