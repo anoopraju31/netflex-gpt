e641ed98dc9fec480f8f087ab276c3c20ab63463
anoopraju31 <anoopraju31@gmail.com>
2023-11-20 12:11:14 +0530

    Skeleton GPT Movie Sugestions Component - refactor

--- src/components/SkeletonGptMovieSuggestion.tsx
+++ src/components/SkeletonGptMovieSuggestion.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import SkeletonCard from './SkeletonCard'
 
 const SkeletonGptMovieSuggestion = () => {
-	return <div>SkeletonGptMovieSuggestion</div>
+	return (
+		<div className='w-full mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-4'>
+			<SkeletonCard />
+			<SkeletonCard />
+			<SkeletonCard />
+			<SkeletonCard />
+			<SkeletonCard />
+			<SkeletonCard />
+			<SkeletonCard />
+			<SkeletonCard />
+			<SkeletonCard />
+			<SkeletonCard />
+			<SkeletonCard />
+			<SkeletonCard />
+		</div>
+	)
 }
 
 export default SkeletonGptMovieSuggestion
