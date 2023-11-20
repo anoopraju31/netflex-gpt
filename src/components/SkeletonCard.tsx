9aa98274c10144eb6e100666ba08c2f9e240591f
anoopraju31 <anoopraju31@gmail.com>
2023-11-20 11:49:02 +0530

    Skeleton Card Component - styled

--- src/components/SkeletonCard.tsx
+++ src/components/SkeletonCard.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
-
 const SkeletonCard = () => {
-	return <div>SkeletonCard</div>
+	return (
+		<div className='rounded-xl w-full animate-pulse bg-black/50'>
+			<div className='w-full h-full bg-white/40 rounded-xl '></div>
+		</div>
+	)
 }
 
 export default SkeletonCard
