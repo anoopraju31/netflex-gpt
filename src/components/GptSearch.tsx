6361c86b5df014f93fbe06e23d578562c9707e8e
anoopraju31 <anoopraju31@gmail.com>
2023-11-20 06:45:16 +0530

    GPT Search Component - form handle submit - initial

--- src/components/GptSearch.tsx
+++ src/components/GptSearch.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import { useRef } from 'react'
 
 const GptSearch = () => {
+	const searchRef = useRef<HTMLInputElement | null>(null)
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+
+		if (!searchRef.current?.value) return
+
+		const searchText = searchRef.current.value
+
+		console.log(searchText)
+		searchRef.current.value = ''
+	}
+
 	return (
 		<div className='pt-20 sm:pt-24 md:pt-28 lg:pt-32 xl:pt-36 2xl:pt-40 flex flex-col items-center'>
-			<form className='flex-shrink-0 p-4 w-full flex justify-center'>
+			<form
+				onSubmit={handleSubmit}
+				className='flex-shrink-0 p-4 w-full flex justify-center'>
 				<input
+					ref={searchRef}
 					type='text'
 					placeholder='What would you like to watch today?'
 					className='w-full max-w-2xl p-3 sm:p-4 rounded-tl-xl rounded-bl-xl text-gray-900 text-sm sm:text-base sm:font-medium outline-none'
