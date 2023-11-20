a5f0067603498e6f01fde6690d966d10ea0e90e3
anoopraju31 <anoopraju31@gmail.com>
2023-11-20 12:10:49 +0530

    Skeleton Card Component - refactor

--- src/components/SkeletonCard.tsx
+++ src/components/SkeletonCard.tsx
@@ -1,6 +1,6 @@
 const SkeletonCard = () => {
 	return (
-		<div className='rounded-xl w-full animate-pulse bg-black/50'>
+		<div className='rounded-xl w-full aspect-[2/3] animate-pulse bg-black/50'>
 			<div className='w-full h-full bg-white/40 rounded-xl '></div>
 		</div>
 	)
