478d6ebeda88283af3248dc077a89dfb5894ba05
anoopraju31 <anoopraju31@gmail.com>
2023-11-20 11:22:54 +0530

    Search Movie List Component - styled

--- src/components/SearchMovieList.tsx
+++ src/components/SearchMovieList.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { useGetMovieByNameQuery } from '../features/movieApi'
+import MovieCard from './MovieCard'
 
-const SearchMovieList = () => {
-	return <div>SearchMovieList</div>
+interface SearchMovieListProps {
+	movie: string
+}
+
+const SearchMovieList = (props: SearchMovieListProps) => {
+	const { movie } = props
+	const { data: movies } = useGetMovieByNameQuery(movie)
+
+	if (!movies) return <div></div>
+
+	return (
+		<>
+			{movies?.map((movie) => (
+				<MovieCard
+					key={movie.id}
+					posterId={movie.poster_path}
+					title={movie.original_title}
+				/>
+			))}
+		</>
+	)
 }
 
 export default SearchMovieList
