3962011552605b60f2f285f352d4144066ef5ea0
anoopraju31 <anoopraju31@gmail.com>
2023-11-21 09:58:28 +0530

    Secondary Movie Container - action, horror, sci-fi, drama, anime, triller movies added

--- src/components/SecondaryMovieContainer.tsx
+++ src/components/SecondaryMovieContainer.tsx
@@ -1,4 +1,5 @@
 import {
+	useGetMovieByGenreQuery,
 	useGetNowPlayingMoviesQuery,
 	useGetPopularMoviesQuery,
 	useGetTopRatedMoviesQuery,
@@ -11,14 +12,25 @@ const SecondaryMovieContainer = () => {
 	const { data: trendingMovies } = useGetTrendingMoviesQuery(1)
 	const { data: popularMovies } = useGetPopularMoviesQuery(1)
 	const { data: topRatedMovies } = useGetTopRatedMoviesQuery(1)
+	const { data: actionMovies } = useGetMovieByGenreQuery(28)
+	const { data: horrorMovies } = useGetMovieByGenreQuery(27)
+	const { data: sciFicMovies } = useGetMovieByGenreQuery(878)
+	const { data: dramaMovies } = useGetMovieByGenreQuery(18)
+	const { data: animeMovies } = useGetMovieByGenreQuery(16)
+	const { data: thrillerMovies } = useGetMovieByGenreQuery(53)
 
 	return (
-		<div className='relative w-full md:-top-10 lg:-top-28 xl:-top-36 z-50'>
+		<div className='relative w-full md:-top-10 lg:-top-28 xl:-top-36 z-50 pb-20'>
 			<MovieList title='Now Playing' movies={nowPlayingMovies} />
 			<MovieList title='Trending' movies={trendingMovies} />
 			<MovieList title='Popular' movies={popularMovies} />
 			<MovieList title='Top Rated' movies={topRatedMovies} />
-			<MovieList title='Now Playing' movies={nowPlayingMovies} />
+			<MovieList title='Action' movies={actionMovies} />
+			<MovieList title='Horror' movies={horrorMovies} />
+			<MovieList title='Sci-Fi' movies={sciFicMovies} />
+			<MovieList title='Drama' movies={dramaMovies} />
+			<MovieList title='Anime' movies={animeMovies} />
+			<MovieList title='Triller' movies={thrillerMovies} />
 		</div>
 	)
 }
